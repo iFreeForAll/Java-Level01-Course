48593be07e5c4651547d983f8eb904226bc724d2
Oleg <guitar_hero@me.com>
2019-09-02 00:44:26 +0200

    Fixed some parts after revision

--- src/Lesson02/HW2.java
+++ src/Lesson02/HW2.java
@@ -5,27 +5,27 @@ import java.util.Arrays;
 public class HW2 {
     public static void main(String[] args) {
         int[] testArray1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        zeroToOneAndOneToZero(testArray1);
+        changeElement(testArray1);
 
         int[] testArray2 = new int[8];
-        arithmeticProgressionArray(testArray2);
+        fillIn(testArray2);
 
         int[] testArray3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        multiplyLessThanSix(testArray3);
+        doubleElement(testArray3);
 
         System.out.println();
         int[][] testArray4 = new int[5][5];
-        diagonalArrayWithOnes(testArray4);
+        crossFill(testArray4);
         System.out.println();
 
         int[] testArray5 = {8, 3, 10, 8, 6, -1, 9, -2};
-        minMaxElement(testArray5);
+        findMinMax(testArray5);
 
         int[] testArray6 = {2, 2, 2, 1, 2, 2, 10, 1};
-        System.out.println(isArrayPartsEqual(testArray6));
+        System.out.println(checkBalance(testArray6));
 
         int[] arrMove = {9, 23, -2, 1, 5, 111, -59, 5};
-        rotateElements(arrMove, -3);
+        shifter(arrMove, -3);
 
         System.out.println();
         int[] arr = {6, 5, 1, 3, 9};
@@ -34,29 +34,23 @@ public class HW2 {
     }
 
     // Here we change elements in array, 0 to 1 and 1 to 0
-    public static void zeroToOneAndOneToZero(int[] arr) {
+    public static void changeElement(int[] arr) {
         for (int i : arr) {
-            if(arr[i] == 0) {
-                arr[i] = 1;
-            } else {
-                arr[i] = 0;
-            }
+            arr[i] = (arr[i] == 1) ? 0 : 1;
         }
         System.out.println(Arrays.toString(arr));
     }
 
     // Here we fill empty array with an arithmetic progression sequence, where n = 3
-    public static void arithmeticProgressionArray(int[] arr) {
-        int num = 0;
-        for (int i : arr) {
-            arr[i] = num;
-            num += 3;
+    public static void fillIn(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 3;
         }
         System.out.println(Arrays.toString(arr));
     }
 
     // Here we multiply each array element that is less than 6 by 2
-    public static void multiplyLessThanSix(int[] arr) {
+    public static void doubleElement(int[] arr) {
         for (int i : arr) {
             if(arr[i] < 6) {
                 arr[i] *= 2;
@@ -66,12 +60,13 @@ public class HW2 {
     }
 
     // Here we fill diagonals of 2D array with 1's
-    public static void diagonalArrayWithOnes(int[][] arr) {
+    public static void crossFill(int[][] arr) {
+        for (int i = 0, j  = arr.length - 1; i < arr.length; i++, j--) {
+            arr[i][j] = 1;
+            arr[i][i] = 1;
+            }
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if(i == j || j == (arr.length - 1 - i)) {
-                    arr[i][j] = 1;
-                }
+            for (int j = 0; j < arr.length; j++) {
                 System.out.print(arr[i][j]);
             }
             System.out.println();
@@ -79,7 +74,7 @@ public class HW2 {
     }
 
     // Here we find max and min elements in the array
-    public static void minMaxElement(int[] arr) {
+    public static void findMinMax(int[] arr) {
         int max = arr[0];
         int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
@@ -92,25 +87,25 @@ public class HW2 {
         System.out.println("Max number is: " + max + ", and min number is: " + min);
     }
 
-    //This method returns true if there's a place in array where the sum of left parts equals to the sum of right parts
-    public static boolean isArrayPartsEqual(int[] arr) {
+    // This method returns true if there's a place in array where the sum of left parts equals to the sum of right parts
+    public static boolean checkBalance(int[] arr) {
         int sumLeft = 0;
-        int sumRight;
+        int sumRight = 0;
         for (int i = 0; i < arr.length; i++) {
             sumLeft += arr[i];
-            sumRight = 0;
             for (int j = i + 1; j < arr.length; j++) {
                 sumRight += arr[j];
             }
             if(sumLeft == sumRight) {
                 return true;
             }
+            sumRight = 0;
         }
         return false;
     }
 
     // This method rotate the elements of array n times
-    public static void rotateElements(int[] arr, int n) {
+    public static void shifter(int[] arr, int n) {
         if(n >= 0) { //check whether we rotate elements to the right or left (to the right here)
             for (int i = 0; i < n; i++) { //indicates how many time we rotate the elements (n times)
                 int lastElement = arr[arr.length - 1]; //store last element of an array for the future
