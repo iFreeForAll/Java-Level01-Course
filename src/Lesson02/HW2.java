93024e5e41d239d1d2851589e20182fc3512a9fa
Oleg <guitar_hero@me.com>
2019-09-01 22:41:21 +0200

    Added "Bubble Sort" method

--- src/Lesson02/HW2.java
+++ src/Lesson02/HW2.java
@@ -26,6 +26,11 @@ public class HW2 {
 
         int[] arrMove = {9, 23, -2, 1, 5, 111, -59, 5};
         rotateElements(arrMove, -3);
+
+        System.out.println();
+        int[] arr = {6, 5, 1, 3, 9};
+        System.out.println("Initial array: " + Arrays.toString(arr));
+        bubbleSort(arr);
     }
 
     // Here we change elements in array, 0 to 1 and 1 to 0
@@ -124,5 +129,20 @@ public class HW2 {
             }
         }
         System.out.println(Arrays.toString(arr));
+    }
+
+    //This method uses "Bubble Sort" algorithm to sort an array
+    public static void bubbleSort(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Array after bubble sort: " + Arrays.toString(arr));
     }
 }
