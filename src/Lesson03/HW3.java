466ed95778d1b0f1f08d54aef835f6a339e0f7f0
Oleg <guitar_hero@me.com>
2019-09-04 17:41:38 +0200

    Fixed some moments after revision

--- src/Lesson03/HW3.java
+++ src/Lesson03/HW3.java
@@ -6,85 +6,88 @@ import java.util.Scanner;
 public class HW3 {
     public static Scanner sc = new Scanner(System.in);
     public static Random rnd = new Random();
+    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
-        //guessTheNumber();
-
-        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
-                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
-                "pepper", "pineapple", "pumpkin", "potato"};
-        guessTheWord(words);
+        guessTheNumber();
+        guessTheWord();
     }
 
-    //First task was to create a simple Guess the Number game
+    //First task was to create a simple 'Guess the Number' game
     //User guesses a random number in range from 0 to 9 and has 3 attempts
     //Whether he loses or wins he can play again or quit
     public static void guessTheNumber() {
         int num = rnd.nextInt(10);
         int guessedNumber;
-        int i = 0;
-        boolean isPlay = true;
+        int i = 1;
+        boolean win = false;
 
         do {
-            System.out.println(num);
             System.out.print("Please guess the number from 0 to 9: ");
             guessedNumber = sc.nextInt();
+
             if (guessedNumber == num) {
                 System.out.println("Yes! I guessed " + num + ", and you got it right! Congratulations!");
-                System.out.println("Wanna try again? 1 - yes, 0 - no: ");
-                int playAgain = sc.nextInt();
-                if (playAgain == 0) {
-                    isPlay = false;
-                } else {
-                    i = 0;
-                    num = rnd.nextInt(10);
-                }
-            } else if (guessedNumber < num && i < 2) {
+                win = playAgain();
+                i = 1;
+                num = rnd.nextInt(10);
+            } else if (guessedNumber < num && i < 3) {
                 System.out.println("My number is bigger than " + guessedNumber + ", try again!");
                 i++;
-            } else if (guessedNumber > num && i < 2) {
+            } else if (guessedNumber > num && i < 3) {
                 System.out.println("My number is lower than " + guessedNumber + ", try again!");
                 i++;
-            } else if (i == 2) {
+            } else {
                 System.out.println("You didn't guess my number in 3 attempts, GAME OVER!");
-                System.out.print("Wanna try again? 1 - yes, 0 - no: ");
-                int playAgain = sc.nextInt();
-                if(playAgain == 0) {
-                    isPlay = false;
-                } else {
-                    i = 0;
-                    num = rnd.nextInt(10);
-                }
+                win = playAgain();
+                i = 1;
+                num = rnd.nextInt(10);
             }
-        } while (isPlay);
+        } while (!win);
     }
 
-    //Second task was to create a Guess the Word game
+    //method that checks if player wants to play again
+    public static boolean playAgain() {
+        System.out.println("Wanna try again? 1 - yes, 0 - no: ");
+        int userPrompt = sc.nextInt();
+        if (userPrompt == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    //Second task was to create a 'Guess the Word' game
     //We have an array of strings, we take a random word from it, and user has to guess the word
     //If the position of the letter in the guessed word is the same as in the random word, user gets a string with such letters
-    public static void guessTheWord (String[] arr) {
-        int rndNum = rnd.nextInt(arr.length);
-        String word = arr[rndNum];
-        String rightPos = "";
+    public static void guessTheWord () {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
 
-        outer: do {
+        int rndNum = rnd.nextInt(words.length);
+        String answer;
+        boolean win = false;
+
+        do {
             System.out.print("Please guess the word: ");
-            String guessedWord = sc.nextLine();
+            answer = sc.nextLine();
 
-            if(word.equals(guessedWord)) {
-                System.out.println("Yes! I guessed the word '" + word + "'. Congratulations!");
-                break outer;
+            if(words[rndNum].equals(answer.toLowerCase())) {
+                System.out.println("Yes! I guessed the word '" + words[rndNum] + "'. Congratulations!");
+                win = true;
             }
 
-            for (int i = 0; i < arr.length && i < guessedWord.length(); i++) {
-                char a = word.charAt(i);
-                char b = guessedWord.charAt(i);
-                if(a == b) {
-                    rightPos += a;
+            if(!win) {
+                for (int i = 0; i < words[rndNum].length(); i++) {
+                    if(answer.length() > i && answer.charAt(i) == words[rndNum].charAt(i)) {
+                        sb.append("(" + answer.charAt(i) + ")");
+                    }
                 }
+
+                System.out.println("You got these letters in the right position: #####" + sb.toString() + "#####");
+                sb.delete(0, sb.length());
+                System.out.println();
             }
-            System.out.println(rightPos + "###############");
-            rightPos = "";
-        } while (true);
+        } while (!win);
     }
 }
